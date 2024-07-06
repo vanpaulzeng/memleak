c4531e68d46ccbb5e90997d05fa9971fe12230e5
PaulXcode <vanpaulzeng@gmail.com>
2024-07-05 22:52:56 -0700

    Initial Commit

---
+++ memleak/IntWrapper.h
@@ -0,0 +1,49 @@
+//
+//  IntWrapper.h
+//  memleak
+//
+//  Created by Admin on 2024-07-05.
+//
+
+//#ifndef IntWrapper_h
+//#define IntWrapper_h
+
+
+
+
+#ifndef INTWRAPPER_INCLUDED
+#define INTWRAPPER_INCLUDED
+
+// IntWrapper simply stores and retrieves an int.  However, every
+// constructor, the destructor, and the assignment operator report
+// when they're called.
+
+class IntWrapper
+{
+public:
+    IntWrapper();
+    IntWrapper(int i);
+    IntWrapper(const IntWrapper& other);
+    ~IntWrapper();
+    IntWrapper& operator=(const IntWrapper& rhs);
+    int value() const;
+private:
+    int m_value;
+};
+
+bool operator==(const IntWrapper& lhs, const IntWrapper& rhs);
+bool operator!=(const IntWrapper& lhs, const IntWrapper& rhs);
+bool operator< (const IntWrapper& lhs, const IntWrapper& rhs);
+bool operator<=(const IntWrapper& lhs, const IntWrapper& rhs);
+bool operator> (const IntWrapper& lhs, const IntWrapper& rhs);
+bool operator>=(const IntWrapper& lhs, const IntWrapper& rhs);
+
+#endif // INTWRAPPER_INCLUDED
+
+
+
+
+
+
+
+//#endif /* IntWrapper_h */
